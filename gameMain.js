7942ceca45474299764882c659fea1ac5196fc59
Will Hinton <hintonw@augsburg.edu>
2017-07-18 19:31:36 -0500

    Added movement for the tiny red square

--- gameMain.js
+++ gameMain.js
@@ -17,16 +17,30 @@ var myGameArea = {
 	    this.canvas.height = canvasHeight;
 	    this.context = this.canvas.getContext("2d");
 	    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		
+		this.interval = setInterval(updateGameArea, 20);
+	},
+	
+	clear : function(){
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	}
 }
 
-function entity(entityWidth, entityHeight, entityColor, spawnX, spawnY){
+function entity(entityWidth, entityHeight, entityColor, x, y){
 	this.entityWidth = entityWidth;
 	this.entityHeight = entityHeight;
-	this.spawnX = spawnX;
-	this.spawnY = spawnY;
+	this.x = x;
+	this.y = y;
 	
-    ctx = myGameArea.context;
-    ctx.fillStyle = entityColor;
-    ctx.fillRect(this.spawnX, this.spawnY, this.entityWidth, this.entityHeight);
+	this.update = function(){
+		ctx = myGameArea.context;
+		ctx.fillStyle = entityColor;
+		ctx.fillRect(this.x, this.y, this.entityWidth, this.entityHeight);
+	}
+}
+
+function updateGameArea(){
+    myGameArea.clear();
+    player.x += 1;
+    player.update();
 }
